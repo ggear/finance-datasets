1bfb340817c9ce49c0a25b6a08421216eda19d63
Graham Gear <gearg1@south32.net>
2019-09-24 08:39:35 +0800

    Update to ignore certififcate verify

--- fx/fx.py
+++ fx/fx.py
@@ -4,7 +4,7 @@ import calendar
 import os.path
 import re
 import urllib2
-
+import ssl
 import datetime
 import pandas as pd
 from gspread_pandas import Spread
@@ -61,7 +61,8 @@ for year in range(ATO_START_YEAR, datetime.datetime.now().year + 1):
             for suffix in ATO_URL_SUFFIX:
                 url = (ATO_URL_PREFIX + suffix).format(month_string, year)
                 try:
-                    month_xls = urllib2.urlopen(urllib2.Request(url, headers=HTTP_HEADER))
+                    month_xls = urllib2.urlopen(urllib2.Request(url, headers=HTTP_HEADER), 
+                    	context=ssl._create_unverified_context())
                     with open(year_month_file, 'wb') as output:
                         output.write(month_xls.read())
                     print("{}-{} downloaded [{}] from [{}]".format(year, str(month).zfill(2), 'TRUE', url))
@@ -114,7 +115,8 @@ for years in RBA_YEARS:
         print("{} cached [{}]".format(years, 'FALSE'))
         url = RBA_URL.format(years)
         try:
-            month_xls = urllib2.urlopen(urllib2.Request(url, headers=HTTP_HEADER))
+            month_xls = urllib2.urlopen(urllib2.Request(url, headers=HTTP_HEADER),
+            	context=ssl._create_unverified_context())
             with open(years_file, 'wb') as output:
                 output.write(month_xls.read())
             print("{} downloaded [{}] from [{}]".format(years, 'TRUE', url))
